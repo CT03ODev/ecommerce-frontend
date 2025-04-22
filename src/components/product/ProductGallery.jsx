9fdaf6a9558eb5b1b731e7c3f680fc8345244000
thanhvo-beready <thanh.vo@beready.academy>
2025-04-22 21:49:35 +0700

    feat(ProductGallery): add image selection and gallery functionality
    Introduce state management for selected image and gallery images. Enhance user interaction by allowing image selection and highlighting the selected image. This improves the product gallery's usability and visual feedback.

--- src/components/product/ProductGallery.jsx
+++ src/components/product/ProductGallery.jsx
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ProductGallery({ product }) {
+    const [images, setImages] = useState([]);
+    const [selectedImage, setSelectedImage] = useState('');
+
+    useEffect(() => {
+        setSelectedImage(product.thumbnail_url);
+        
+        const allImages = [];
+        
+        allImages.push(product.thumbnail_url);
+        
+        if (product.variants && product.variants.length > 0) {
+            product.variants.forEach(variant => {
+                if (variant.images && variant.images.length > 0) {
+                    variant.images.forEach(image => {
+                        // Kiểm tra trùng lặp trước khi thêm vào
+                        if (!allImages.includes(image)) {
+                            allImages.push(image);
+                        }
+                    });
+                }
+            });
+        }
+        setImages(allImages);
+    }, [product]);
+
+    const handleImageClick = (image) => {
+        setSelectedImage(image);
+    };
+
     return (
         <div>
-            <img src={product.thumbnail_url} alt={product.slug} className="w-full" />
+            <div className="w-full overflow-hidden rounded-lg">
+                <img 
+                    src={selectedImage} 
+                    alt={product.name} 
+                    className="w-full object-cover"
+                />
+            </div>
+            
             <div className="grid grid-cols-5 gap-4 mt-4">
-                {product.images.map((image, index) => (
+                {images.map((image, index) => (
                     <img
                         key={index}
                         src={image}
                         alt={`Gallery ${index}`}
-                        className="w-full cursor-pointer border"
+                        className={`w-full cursor-pointer border hover:border-blue-500 ${selectedImage === image ? 'border-blue-500 border-2' : 'border-gray-200'}`}
+                        onClick={() => handleImageClick(image)}
                     />
                 ))}
             </div>
